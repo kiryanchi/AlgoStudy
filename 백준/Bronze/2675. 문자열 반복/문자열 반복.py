70e12eda776aadf8b13d1dd16dcb46c036b8b228
박기현 <50714602+kiryanchi@users.noreply.github.com>
2022-11-11 11:03:33 +0900

    [Bronze II] Title: 문자열 반복, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        R, S = input().split()
+        R = int(R)
+        
+        for s in S:
+            print(s*R,end="")
+        print()
+    
