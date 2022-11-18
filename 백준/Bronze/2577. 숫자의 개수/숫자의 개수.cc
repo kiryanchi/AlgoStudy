7328428c2d0502629ba94689f7a0cd1e14339099
박기현 <50714602+kiryanchi@users.noreply.github.com>
2022-11-18 17:44:53 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,27 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int used[10];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    
+    int number = a * b * c;
+    
+    while (number > 0) {
+        used[number % 10]++;
+        number = number / 10;
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        cout << used[i] << '\n';
+    }
+}
