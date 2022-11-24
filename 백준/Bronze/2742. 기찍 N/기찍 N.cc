b531c26ae0973a77c6720d7b5e1a3b6a7986f3bc
박기현 <50714602+kiryanchi@users.noreply.github.com>
2022-11-24 17:48:09 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N;
+    
+    for (int i = N; i > 0; i--) {
+        cout << i << '\n';
+    }
+    
+    return 0;
+}
